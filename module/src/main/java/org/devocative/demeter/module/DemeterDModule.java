4da72b93037ec2486492b3af5a8748047e977141
Mehdi Bizhani <mbizhani@gmail.com>
2016-01-15 00:12:22 +0330

    - DemeterDModule as other module is defined - Demeter.xml added

---
+++ module/src/main/java/org/devocative/demeter/module/DemeterDModule.java
@@ -0,0 +1,15 @@
+package org.devocative.demeter.module;
+
+import org.devocative.demeter.imodule.DModule;
+
+public class DemeterDModule implements DModule {
+	@Override
+	public void onInit() {
+
+	}
+
+	@Override
+	public void onShutdown() {
+
+	}
+}
