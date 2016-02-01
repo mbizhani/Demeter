b880fa5725cbdd954e2dfad2db56f26653bb2483
Mehdi Bizhani <mbizhani@gmail.com>
2016-02-01 09:46:13 +0330

    - two url redirection-related methods

--- web/src/main/java/org/devocative/demeter/web/DPage.java
+++ web/src/main/java/org/devocative/demeter/web/DPage.java
@@ -1,11 +1,31 @@
 package org.devocative.demeter.web;
 
 import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.request.UrlUtils;
+import org.apache.wicket.request.cycle.RequestCycle;
+import org.apache.wicket.request.flow.RedirectToUrlException;
+import org.devocative.demeter.iservice.IPageService;
 
+import javax.inject.Inject;
 import java.util.List;
 
 public abstract class DPage extends Panel {
+	@Inject
+	private IPageService pageService;
+
 	public DPage(String id, List<String> params) {
 		super(id);
+	}
+
+	protected void redirectToDPage(Class<? extends DPage> dPageClass) {
+		throw new RedirectToUrlException(getUriForPage(dPageClass));
+	}
+
+	protected String getUriForPage(Class<? extends DPage> dPageClass) {
+		String uriByPage = pageService.getUriByPage(dPageClass);
+		if (uriByPage.length() > 0 && uriByPage.charAt(0) == '/') {
+			uriByPage = uriByPage.substring(1);
+		}
+		return UrlUtils.rewriteToContextRelative(uriByPage, RequestCycle.get());
 	}
 }
