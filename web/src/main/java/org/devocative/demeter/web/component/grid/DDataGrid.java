f27cecedd358dcbd1073ced682cadcbbd6e6544e
Mehdi Bizhani <mbizhani@gmail.com>
2016-03-11 15:16:45 +0330

    - based on some refactoring in Wickomp's grid components

--- web/src/main/java/org/devocative/demeter/web/component/grid/DDataGrid.java
+++ web/src/main/java/org/devocative/demeter/web/component/grid/DDataGrid.java
@@ -1,12 +1,12 @@
 package org.devocative.demeter.web.component.grid;
 
 import org.devocative.demeter.web.DemeterExceptionToMessageHandler;
-import org.devocative.wickomp.data.WGridDataSource;
+import org.devocative.wickomp.grid.IGridDataSource;
 import org.devocative.wickomp.grid.OGrid;
 import org.devocative.wickomp.grid.WDataGrid;
 
 public class DDataGrid<T> extends WDataGrid<T> {
-	public DDataGrid(String id, OGrid<T> options, WGridDataSource<T> dataSource) {
+	public DDataGrid(String id, OGrid<T> options, IGridDataSource<T> dataSource) {
 		super(id, options, dataSource);
 
 		setExceptionMessageHandler(DemeterExceptionToMessageHandler.get());
--- web/src/main/java/org/devocative/demeter/web/component/grid/DTreeGrid.java
+++ web/src/main/java/org/devocative/demeter/web/component/grid/DTreeGrid.java
@@ -1,12 +1,12 @@
 package org.devocative.demeter.web.component.grid;
 
 import org.devocative.demeter.web.DemeterExceptionToMessageHandler;
-import org.devocative.wickomp.data.WTreeGridDataSource;
+import org.devocative.wickomp.grid.ITreeGridDataSource;
 import org.devocative.wickomp.grid.OTreeGrid;
 import org.devocative.wickomp.grid.WTreeGrid;
 
 public class DTreeGrid<T> extends WTreeGrid<T> {
-	public DTreeGrid(String id, OTreeGrid<T> options, WTreeGridDataSource<T> dataSource) {
+	public DTreeGrid(String id, OTreeGrid<T> options, ITreeGridDataSource<T> dataSource) {
 		super(id, options, dataSource);
 
 		setExceptionMessageHandler(DemeterExceptionToMessageHandler.get());
