86c65f16e12643b297756b63cfc76dff59cba7dc
Mehdi Bizhani <mbizhani@gmail.com>
2016-06-15 17:08:05 +0430

    refactored to support both BASIC & DIGEST

--- web/src/main/java/org/devocative/demeter/web/http/DemeterHttpAuthFilter.java
+++ web/src/main/java/org/devocative/demeter/web/http/DemeterHttpAuthFilter.java
@@ -2,10 +2,12 @@ package org.devocative.demeter.web.http;
 
 import org.devocative.adroit.ConfigUtil;
 import org.devocative.demeter.DemeterConfigKey;
+import org.devocative.demeter.DemeterException;
 import org.devocative.demeter.core.ModuleLoader;
 import org.devocative.demeter.iservice.ISecurityService;
 import org.devocative.demeter.vo.UserVO;
-import org.devocative.wickomp.http.filter.WBaseHttpDigestAuthFilter;
+import org.devocative.wickomp.http.filter.WAuthMethod;
+import org.devocative.wickomp.http.filter.WBaseHttpAuthFilter;
 import org.devocative.wickomp.http.filter.WHttpAuthBean;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,15 +19,15 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
-public class DemeterDigestAuthFilter extends WBaseHttpDigestAuthFilter {
-	private static final Logger logger = LoggerFactory.getLogger(DemeterDigestAuthFilter.class);
+public class DemeterHttpAuthFilter extends WBaseHttpAuthFilter {
+	private static final Logger logger = LoggerFactory.getLogger(DemeterHttpAuthFilter.class);
 
 	private String nonce;
 	private ScheduledExecutorService nonceRefreshExecutor;
 	private ISecurityService securityService;
 
 	@Override
-	protected String calculateNonce(WHttpAuthBean authBean) {
+	protected String calculateNonceForDigest(WHttpAuthBean authBean) {
 		return nonce;
 	}
 
@@ -35,29 +37,46 @@ public class DemeterDigestAuthFilter extends WBaseHttpDigestAuthFilter {
 	}
 
 	@Override
-	protected String generateUserHash(WHttpAuthBean authBean) {
+	protected String generateUserHashForDigest(WHttpAuthBean authBean) {
 		return securityService.getUserDigest(authBean.getUsername());
 	}
 
 	@Override
+	protected boolean authenticateByPasswordForBasic(String username, String password) {
+		try {
+			securityService.authenticate(username, password);
+			return true;
+		} catch (DemeterException e) {
+			return false;
+		}
+	}
+
+	@Override
 	public void init(FilterConfig filterConfig) throws ServletException {
-		nonce = UUID.randomUUID().toString();
+		String httpMode = ConfigUtil.getString(DemeterConfigKey.HttpAuthenticationMode);
+		if ("digest".equals(httpMode)) {
+			setDesiredAuthMethod(WAuthMethod.DIGEST);
+		}
 
-		nonceRefreshExecutor = Executors.newScheduledThreadPool(1);
+		if (getDesiredAuthMethod() == WAuthMethod.DIGEST) {
+			nonce = UUID.randomUUID().toString();
 
-		nonceRefreshExecutor.scheduleAtFixedRate(new Runnable() {
+			nonceRefreshExecutor = Executors.newScheduledThreadPool(1);
 
-			public void run() {
-				nonce = UUID.randomUUID().toString();
-			}
-		}, 1, 1, TimeUnit.MINUTES);
+			nonceRefreshExecutor.scheduleAtFixedRate(new Runnable() {
+
+				public void run() {
+					nonce = UUID.randomUUID().toString();
+				}
+			}, 1, 1, TimeUnit.MINUTES);
+		}
 
 		securityService = ModuleLoader.getApplicationContext().getBean(ISecurityService.class);
 
 		setProcessAuth(ConfigUtil.getBoolean(DemeterConfigKey.EnabledSecurity));
 
-		logger.info("DemeterDigestAuthFilter Inited: DoAuth =[{}]",
-			ConfigUtil.getBoolean(DemeterConfigKey.EnabledSecurity));
+		logger.info("DemeterHttpAuthFilter Inited: DoAuth =[{}] Method=[{}]",
+			ConfigUtil.getBoolean(DemeterConfigKey.EnabledSecurity), getDesiredAuthMethod());
 	}
 
 	@Override
