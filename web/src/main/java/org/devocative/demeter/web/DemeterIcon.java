315d2b6b44ef0f064605cb5c3a5e8bf47eb3c660
Mehdi Bizhani <mbizhani@gmail.com>
2016-10-27 17:38:02 +0330

    * Using IconFont instead of HTMLBase for Icon

--- web/src/main/java/org/devocative/demeter/web/DemeterIcon.java
+++ web/src/main/java/org/devocative/demeter/web/DemeterIcon.java
@@ -1,21 +1,21 @@
 package org.devocative.demeter.web;
 
 import org.apache.wicket.model.ResourceModel;
-import org.devocative.wickomp.html.HTMLBase;
 import org.devocative.wickomp.html.icon.FontAwesome;
+import org.devocative.wickomp.html.icon.IconFont;
 
 public class DemeterIcon {
-	public static final HTMLBase EDIT = new FontAwesome("pencil", new ResourceModel("label.edit", "Edit"));
-	public static final HTMLBase EXECUTE = new FontAwesome("cogs", new ResourceModel("label.execute", "Execute")).setStyleClass("ic-imp");
-	public static final HTMLBase SEARCH = new FontAwesome("search", new ResourceModel("label.search", "Search"));
-	public static final HTMLBase SAVE = new FontAwesome("floppy-o", new ResourceModel("label.save", "Save"));
-	public static final HTMLBase REMOVE = new FontAwesome("times", new ResourceModel("label.remove", "Remove")).setStyleClass("ic-false");
-	public static final HTMLBase ADD = new FontAwesome("plus", new ResourceModel("label.add", "Add"));
+	public static final IconFont EDIT = new FontAwesome("pencil", new ResourceModel("label.edit", "Edit"));
+	public static final IconFont EXECUTE = new FontAwesome("cogs", new ResourceModel("label.execute", "Execute")).setStyleClass("ic-imp");
+	public static final IconFont SEARCH = new FontAwesome("search", new ResourceModel("label.search", "Search"));
+	public static final IconFont SAVE = new FontAwesome("floppy-o", new ResourceModel("label.save", "Save"));
+	public static final IconFont REMOVE = new FontAwesome("times", new ResourceModel("label.remove", "Remove")).setStyleClass("ic-false");
+	public static final IconFont ADD = new FontAwesome("plus", new ResourceModel("label.add", "Add"));
 
-	public static final HTMLBase EXPORT_EXCEL = new FontAwesome("file-excel-o", new ResourceModel("label.export.excel", "Export Excel")).setStyleClass("ic-imp");
-	public static final HTMLBase EXPAND = new FontAwesome("expand", new ResourceModel("label.nodes.expand", "Expand"));
-	public static final HTMLBase COLLAPSE = new FontAwesome("compress", new ResourceModel("label.nodes.collapse", "Collapse"));
+	public static final IconFont EXPORT_EXCEL = new FontAwesome("file-excel-o", new ResourceModel("label.export.excel", "Export Excel")).setStyleClass("ic-imp");
+	public static final IconFont EXPAND = new FontAwesome("expand", new ResourceModel("label.nodes.expand", "Expand"));
+	public static final IconFont COLLAPSE = new FontAwesome("compress", new ResourceModel("label.nodes.collapse", "Collapse"));
 
-	public static final HTMLBase TRUE = new FontAwesome("check", new ResourceModel("label.true", "True")).setStyleClass("ic-true");
-	public static final HTMLBase FALSE = new FontAwesome("times", new ResourceModel("label.false", "False")).setStyleClass("ic-false");
+	public static final IconFont TRUE = new FontAwesome("check", new ResourceModel("label.true", "True")).setStyleClass("ic-true");
+	public static final IconFont FALSE = new FontAwesome("times", new ResourceModel("label.false", "False")).setStyleClass("ic-false");
 }
