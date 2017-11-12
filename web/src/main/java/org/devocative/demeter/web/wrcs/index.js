731444117c4c471d3382a5e727321e262a7db06d
Mehdi Bizhani <mbizhani@gmail.com>
2017-11-12 14:48:29 +0330

    improve session timeout and ws-ping

--- web/src/main/java/org/devocative/demeter/web/wrcs/index.js
+++ web/src/main/java/org/devocative/demeter/web/wrcs/index.js
@@ -1,6 +1,6 @@
-var webSocketPingHandler;
-var idleSessionCounter = 0;
 var confirmDlg = null;
+var wsPingHandler = null;
+var idleSessionCounter = 0;
 
 wLog.info("Demeter Init: WebSocketPing=[" + WebSocketPingInterval + "] SessionTO=[" + sessionTO + "]");
 
@@ -8,6 +8,7 @@ hideBusySign();
 
 Wicket.Event.subscribe("/ajax/call/beforeSend", function (attributes, jqXHR, settings) {
 	showBusySign();
+	confirmDlg = null;
 	idleSessionCounter = 0;
 });
 Wicket.Event.subscribe("/ajax/call/complete", function (attributes, jqXHR, textStatus) {
@@ -22,26 +23,26 @@ Wicket.Event.subscribe("/websocket/open", function (jqEvent) {
 	wLog.info("Demeter: websocket/open");
 
 	if (WebSocketPingInterval) {
-		webSocketPingHandler = setInterval(pingWebSocket, WebSocketPingInterval * 1000);
+		wsPingHandler = setInterval(pingWebSocket, WebSocketPingInterval * 1000);
 	}
 });
 Wicket.Event.subscribe("/websocket/closed", function (jqEvent) {
 	wLog.warn("Demeter: websocket/closed", jqEvent);
-	clearInterval(webSocketPingHandler);
+	clearInterval(wsPingHandler);
 });
 Wicket.Event.subscribe("/websocket/error", function (jqEvent) {
 	wLog.error("Demeter: websocket/error", jqEvent);
 });
 Wicket.Event.subscribe("/websocket/message", function (jqEvent, message) {
 	if (message && message != "W.R_PING") {
-		if (sessionTO && sessionTO > 0 && idleSessionCounter > (sessionTO - 35)) {
+		if (sessionTO > 0 && idleSessionCounter > (sessionTO - 35)) {
 			wLog.info("Demeter: resend ajax on ws response, reset idle");
 			Wicket.Ajax.get({u: ajaxUrl});
 		}
 	}
 });
 
-if (sessionTO && sessionTO > 0) {
+if (sessionTO > 0) {
 	setInterval(processIdleSession, 1000);
 }
 
@@ -64,7 +65,6 @@ function processIdleSession() {
 		wLog.warn("30sec Before Expiration!");
 
 		confirmDlg = $.messager.confirm("", "Session is to expired. Reconnect?", function (r) {
-			confirmDlg = null;
 			if (r) {
 				Wicket.Ajax.get({u: ajaxUrl});
 			}
