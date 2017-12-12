3f95de1ac3391594fa6b4d95b29e698ced4a070d
Mehdi Bizhani <mbizhani@gmail.com>
2017-12-12 11:36:04 +0330

    adding icon "shield"

--- web/src/main/java/org/devocative/demeter/web/DemeterIcon.java
+++ web/src/main/java/org/devocative/demeter/web/DemeterIcon.java
@@ -7,7 +7,6 @@ import org.devocative.wickomp.html.icon.IconFont;
 public class DemeterIcon {
 	public static final IconFont EDIT = new FontAwesome("pencil", new ResourceModel("label.edit", "Edit"));
 	public static final IconFont EXECUTE = new FontAwesome("cogs", new ResourceModel("label.execute", "Execute")).setStyleClass("ic-imp");
-	public static final IconFont SEARCH = new FontAwesome("search", new ResourceModel("label.search", "Search"));
 	public static final IconFont SAVE = new FontAwesome("floppy-o", new ResourceModel("label.save", "Save"));
 	public static final IconFont REMOVE = new FontAwesome("times", new ResourceModel("label.remove", "Remove")).setStyleClass("ic-false");
 
@@ -15,19 +14,23 @@ public class DemeterIcon {
 	public static final IconFont ADD_USER = new FontAwesome("user-plus", new ResourceModel("label.add.user", "Add User"));
 
 	public static final IconFont EXPORT_EXCEL = new FontAwesome("file-excel-o", new ResourceModel("label.export.excel", "Export Excel")).setStyleClass("ic-imp");
-	public static final IconFont EXPAND = new FontAwesome("expand", new ResourceModel("label.nodes.expand", "Expand"));
 	public static final IconFont COLLAPSE = new FontAwesome("compress", new ResourceModel("label.nodes.collapse", "Collapse"));
 
 	public static final IconFont TRUE = new FontAwesome("check", new ResourceModel("label.true", "True")).setStyleClass("ic-true");
 	public static final IconFont FALSE = new FontAwesome("times", new ResourceModel("label.false", "False")).setStyleClass("ic-false");
 
-	public static final IconFont DOWNLOAD = new FontAwesome("download", new ResourceModel("label.download", "Download"));
 	public static final IconFont ATTACHMENT = new FontAwesome("paperclip", new ResourceModel("label.attachment", "Attachment"));
 	public static final IconFont INFO = new FontAwesome("bug", new ResourceModel("label.info", "Info"));
 
 	public static final IconFont SHOW = new FontAwesome("eye", new ResourceModel("label.show", "Show"));
 
-	public static final IconFont UPLOAD = new FontAwesome("upload", new ResourceModel("label.upload", "Upload"));
-
 	public static final IconFont EXPORT_IMPORT = new FontAwesome("exchange", new ResourceModel("label.exchange", "Export/Import"));
+
+	// ---------- THE SAME NAME
+
+	public static final IconFont DOWNLOAD = new FontAwesome("download", new ResourceModel("label.download", "Download"));
+	public static final IconFont EXPAND = new FontAwesome("expand", new ResourceModel("label.nodes.expand", "Expand"));
+	public static final IconFont SEARCH = new FontAwesome("search", new ResourceModel("label.search", "Search"));
+	public static final IconFont SHIELD = new FontAwesome("shield", new ResourceModel("label.shield", "Shield"));
+	public static final IconFont UPLOAD = new FontAwesome("upload", new ResourceModel("label.upload", "Upload"));
 }
